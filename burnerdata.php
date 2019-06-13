1f4e999e7f15baa3b34e7a3bdf62db5427432dca
Charles Dauer <cdauer@Truman.local>
2019-06-13 11:10:56 -0500

    initial commit and creation for version 2

---
+++ burnerdata.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="refresh" content="60" />
+		<title>Ember&trade; A Smart Combustion&trade; Solution</title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<?php include('burner-functions.php'); ?>
+	<body>
+		<div class="row">
+				<div id="header" class="col align-items-center">
+					<h1>Ember</h1>
+					<span class="tradem">&trade;</span>
+					<p class="tagline">A Smart Combustion</p><span class="tradem tradem-sm">&trade;</span> <p class="tagline tag-end">Solution</p>
+					<a href="zoloscan.php" role="button" class="page-links btn btn-light">ZoloSCAN</a><a href="burnerdata.php" role="button" class="page-links btn btn-light">BurnerDATA</a>
+				</div>
+			</div>
+		<div id="sections" class="row align-items-center">
+				<p>&lambda;</p>
+		</div>
+	</body>
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.bundle.js"></script>
+</html>
